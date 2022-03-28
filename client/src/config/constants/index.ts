4a9c3cf81c920b6f02c8f6b4a4624ea9649c882d
wokeydev <whitedeveloper0203@gmail.com>
2022-03-28 13:46:55 -0600

    Use uniswap v2 router address

--- client/src/config/constants/index.ts
+++ client/src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@uniswap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
 // a list of tokens by chain
 type ChainTokenList = {
